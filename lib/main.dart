adc7929f5042a83ed9ca84218b2ecf8c61c5b56b
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2025-11-24 16:37:45 +0300

    create:the app bar widget and added text

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,28 @@
 import 'package:flutter/material.dart';
 
+void main(){
+    runApp (const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+     const MyApp ({super.key});
+
+     @override
+        Widget build (BuildContext context) {
+
+            return MaterialApp (
+                home:Scaffold(
+                  appBar: AppBar(  
+                    title:Text("Hello Flutter"),
+                    backgroundColor: Colors.blue,
+                ),
+                
+                  body: Container(),   
+                ),
+            );
+        }
+
+
+
+
+}
