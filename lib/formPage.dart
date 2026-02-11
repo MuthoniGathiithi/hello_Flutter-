4214107148dcac42ded9a6f91f18db6ec4b5b129
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2026-02-11 23:57:30 +0300

    add button and second page

---
+++ lib/formPage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form Page'),
+      ),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Name",
+                hintText: "Enter your name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'formPage.dart';
 
 void main(){
-    runApp (const MyApp());
+runApp (MainApp());
+
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+const MainApp ({super.key});
+
+@override
+
+Widget build (BuildContext context){
+ return MaterialApp(
+  home: HomePage(),
+ );
+}
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Get Started",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+              child: Text("Get Started"),
+            ),
+          ],
         ),
       ),
     );
