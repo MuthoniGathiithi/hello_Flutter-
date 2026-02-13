73b98143472d697733ecaf4a640286d11ab162d7
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2026-02-13 23:53:16 +0300

    improve : ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,19 @@
 import 'package:flutter/material.dart';
 import 'formPage.dart';
 
-void main(){
-runApp (MainApp());
-
+void main (){
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-const MainApp ({super.key});
 
-@override
-
-Widget build (BuildContext context){
- return MaterialApp(
-  home: HomePage(),
- );
-}
-}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  material Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: Column(
